bfe4e0bd59c67b457f1504d57d8768307dd4d96f
hamed ebrahimimi <hamedebrahimi128@gmail.com>
2024-06-02 10:56:44 +0330

    fix bog and change state date

--- stories/components/datepiker/calendar/index.tsx
+++ stories/components/datepiker/calendar/index.tsx
@@ -7,56 +7,74 @@ import { useState } from "react";
 const Calendar = (props: DatePikerProps) => {
     const [showMonth, setShowMonth] = useState(false)
     const [showYear, setShowYear] = useState(false)
-    const daysMonth = new DateObject({ calendar: props.calendar === 'persian' ? persian : undefined, locale: persian_fa }).toLastOfMonth().day
-    const daysArray = Array.from({ length: daysMonth }, (_, index) => index + 1);
-    const startDay = new DateObject({ calendar: props.calendar === 'persian' ? persian : undefined, locale: persian_fa }).toFirstOfMonth().weekDay.index
-    const calendarArray = Array(42).fill(null);
-    for (let i = 0; i < daysArray.length; i++) {
-        calendarArray[startDay + i] = daysArray[i];
-    }
-    const [date , setDate] = useState(new DateObject({ calendar: props.calendar === 'persian' ? persian : undefined, locale: persian_fa }))
+    const [currentMonth, setCurrentMonth] = useState(new DateObject({
+        calendar: props.calendar === 'persian' ? persian : undefined,
+        locale: persian_fa
+    }).toFirstOfMonth());
+
+    const today = new DateObject({
+        calendar: props.calendar === 'persian' ? persian : undefined,
+        locale: persian_fa
+    });
+
+    const nextMonth = () => {
+        setCurrentMonth(currentMonth.add(1, "month").toFirstOfMonth());
+    };
+
+    const prevMonth = () => {
+        setCurrentMonth(currentMonth.subtract(1, "month").toFirstOfMonth());
+    };
     return (
         <div className="w-1/3 p-4 bg-gray-200 rounded-lg shadow-sm">
             <div className="w-full py-2 flex items-center justify-center gap-3 text-xl font-medium">
                 <p onClick={() => {setShowMonth(true) ; setShowYear(false) }}>
                     {
-                        date.month.name
+                        currentMonth.month.name
                     }
                 </p>
                 <p onClick={() => {setShowMonth(false) ; setShowYear(true) }}>
                     {
-                        date.year
+                        today.year
                     }
                 </p>
             </div>
             {
                 !showMonth && !showYear && <table className="w-full">
                     <thead>
-                        <tr>
-                            {
-                                props.weekDayString ? props.weekDayString?.map(item => (<th key={item}>{item}</th>)) : date.weekDays.map(item => (<th key={item.index}>{item.name}</th>))
-                            }
-                        </tr>
+                    <tr>
+                        {
+                            props.weekDayString ? props.weekDayString?.map(item => (
+                                <th key={item}>{item}</th>)) : today.weekDays.map(item => (
+                                <th key={item.index}>{item.name}</th>))
+                        }
+                    </tr>
                     </thead>
 
                     <tbody>
-                        {Array.from({ length: 6 }).map((_, weekIndex) => (
-                            <tr key={weekIndex}>
-                                {Array.from({ length: 7 }).map((_, dayIndex) => {
-                                    const day = date.set('day' , weekIndex * 7 + dayIndex)
+                    {today.weekDays.map((weekDay, weekIndex) => (
+                        <tr key={weekIndex}>
+                            {Array.from({ length: 7 }).map((_, dayIndex) => {
+                                const day = new DateObject(currentMonth).add(weekIndex * 7 + dayIndex - firstOfMonth.weekDay.index, 'day');
+                                const isDisabled = props.disablingThePreviousDay ? day.valueOf() < today.valueOf() : false;
 
-                                    return <td  className="text-center rounded-full hover:bg-blue-300" key={dayIndex}><Day key={dayIndex} disable={props.disablingThePreviousDay ? date.valueOf() < new DateObject({ calendar: props.calendar === 'persian' ? persian : undefined, locale: persian_fa }).valueOf() : false} date={day}/></td>;
-                                })}
-                            </tr>
-                        ))}
+                                return (
+                                    <td className="text-center rounded-full hover:bg-blue-300" key={dayIndex}>
+                                        <Day key={dayIndex} disable={isDisabled} date={day} />
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    ))}
                     </tbody>
-
                 </table>
             }
             {
                 showMonth && <div className="w-full grid grid-cols-4">
                     {
-                        new DateObject({ calendar: props.calendar === 'persian' ? persian : undefined, locale: props.calendar === 'persian' ? persian_fa : undefined }).months.map(month => (
+                        new DateObject({
+                            calendar: props.calendar === 'persian' ? persian : undefined,
+                            locale: props.calendar === 'persian' ? persian_fa : undefined
+                        }).months.map(month => (
                             <p key={month.index} className="text-lg font-medium">
                                 {
                                     month.name
